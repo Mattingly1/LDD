69c45149e955d76495cde2741c7e5c91920b9b8a
Mattingly Marinaccio <ma193633@ucf.edu>
2025-01-29 16:47:41 -0500

    changed refresh rate

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -10,7 +10,7 @@ class Dashboard extends React.Component{
         this.state={
             donations: [],
             pollingCount: 0,
-            delay: 1000,
+            delay: 250,
             teamLeaders: [],
             oldDonations: [],
             bigDonation: null
